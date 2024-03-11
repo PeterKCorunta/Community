5cf8e23702bfafb345078ca507bb51ae8c82ebaa
Your Name~ <oruntakelechi86@gmail.com>
2024-03-11 02:58:56 +0100

    New updates on the App.css and Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 // MY COMMUNITY HEADER COMPONENTS IN REACT
 
 function Header() {
+  const [toggle, setIsToggle] = useState(false)
+  const handletoggle = () =>{
+    setIsToggle(!toggle)
+  }
+  const [containerHeight, setContainerHeight] = useState(0)
+
+  useEffect(()=>{
+    const originalwidth = document.querySelector('.App').clientWidth
+    const nav_length = document.querySelector('.nav_header').children.length
+    const newheight = originalwidth===458? document.querySelector('.nav_header').clientHeight + (50 * nav_length) : document.querySelector('.nav_header').clientHeight
+    setContainerHeight(newheight)
+  }, [containerHeight])
+
   return (
     <header className='community_header'>
         <h1 className='community_title'>My Community</h1>
-        <ul className='nav_header'>
+        <ul className='nav_header' style={toggle? {height: `${containerHeight}px`, transition: 'height 1s ease'}:{height:'0%', transition: 'height 1s ease'}}>
             <li><a href='#'>Home</a></li>
             <li><a href='#'>History</a></li>
             <li><a href='#'>Projects</a></li>
         </ul>
-        <h2 className="logo-icon">🛒</h2>
+        <h2 className="logo-icon" onClick={handletoggle}>🛒</h2>
     </header>
   )
 }
