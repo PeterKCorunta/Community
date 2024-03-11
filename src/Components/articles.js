c2c9bdd6562e6918af8a58b6761d3a9f64613c45
Your Name~ <oruntakelechi86@gmail.com>
2024-03-12 00:39:48 +0100

    First Commit of the export articles array object

---
+++ src/Components/articles.js
@@ -0,0 +1,6 @@
+export const articles = [
+    {id:1, title:"The Ancestral Lineagae", author:"Micheal J. Finch", description:"Reaching forth for the goal and holding onto the past will end up keeping one motionless"},
+    {id:2, title:"The Living Legacy", author:"Timothy Johns", description:"Living the best of today leaves us giving our best for tommorow"},
+    {id:3, title:"The Journey", author:"Anonymous", description:"The journey of a thousand miles begins with a single step"},
+    {id:4, title:"Time to Remember", author:"John Williams", description:"A good memory is like a key to the soul"},
+]
