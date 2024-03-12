23d7ad3dc096891c1aacc0ae2ad2ea22f0cf8666
Your Name~ <oruntakelechi86@gmail.com>
2024-03-12 19:37:20 +0100

    Recent Updates by 7:37pm

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Article({article}) {
   return (
     <div className='article_frame'>
-        <h2>{article.id}</h2>
-        <h3>{article.title}</h3>
+        <h2 className='article_id'>{article.id}</h2>
+        <h3 className='article_title'><a className='article_linktitle' href='#article'>{article.title}</a></h3>
         <p className='author_description'>{article.description}</p>
         <h3 className='author_title'>{article.author}</h3>
     </div>
