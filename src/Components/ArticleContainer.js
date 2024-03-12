7cf97fd5e3f3fde0f06c7610b71eba6d95514d1b
Your Name~ <oruntakelechi86@gmail.com>
2024-03-13 00:03:16 +0100

    ArticleContainer Component Update early hrs on Wednesday 13/03

--- src/Components/ArticleContainer.js
+++ src/Components/ArticleContainer.js
@@ -4,7 +4,7 @@ import Article from './Article'
 import { useState, useEffect } from 'react'
 import { RatioContext } from './RatioContext'
 
-function ArticleContainer({initialWidth}) {
+function ArticleContainer({initialWidth, handleSearch}) {
 
     const [selectedid, setSelectedId] = useState(0);
     const [selectedtext, setSelectedText] = useState("");
@@ -13,16 +13,16 @@ function ArticleContainer({initialWidth}) {
 
     function searchTitle(){
         [...articles].filter(i=>{
-            return (i.title.toLowerCase().includes(selectedtext.toLowerCase())) && setSelectedId(i.id>1?i.id-2:i.id-1)})
+            return (i.title.toLowerCase().includes(selectedtext.toLowerCase())) && setSelectedId(i.id>1?(client_width>=2)?i.id-2:i.id-1:(i.id-1))})
     }
     
   return (
 
     <div className='articlecontainer'>
         <nav className='nav_article'>
-            <h2 className='myarticle'>{client_width}</h2>
-            <input type='text' onChange={(e)=>{setSelectedText(e.target.value)}} value={selectedtext} />
-            <button onClick={searchTitle}>Search</button>
+            <h2 className='myarticle'></h2>
+            <input className='search_title' type='text' placeholder="Enter title" onChange={(e)=>{setSelectedText(e.target.value)}} value={selectedtext} />
+            <button className='searchBtn' onClick={searchTitle}>Search</button>
             <legend>
                 <button onClick={()=>{(selectedid>=1) && setSelectedId(prev=>prev-1)}}>-</button>
                 <button onClick={()=>{(selectedid<articles.length - client_width) && setSelectedId(prev=>prev+1)}}>+</button>
@@ -30,10 +30,10 @@ function ArticleContainer({initialWidth}) {
         </nav>
         <div className='article_flexcontainer'>
             {articles.map((i, index)=>{
-                return <div style={selectedid?
+                return <div key={i.id} style={selectedid?
                      {transform:`translateX(${selectedid * -100}%)`, transition: `transform 1s ease`}
                     :{transform:`translateX(${selectedid * 100}%)`, transition: `transform 1s ease`}}>
-                            <Article key={i.author} article={i}/>
+                            <Article key={i.author} article={i} handleSearch={handleSearch}/>
                        </div>
             })}  
         </div>
