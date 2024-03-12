cb74990abc4e15e6e1165aca530fc25d7878bcb0
Your Name~ <oruntakelechi86@gmail.com>
2024-03-12 16:50:44 +0100

    Latest Update on the ArticleContainer.js

--- src/Components/ArticleContainer.js
+++ src/Components/ArticleContainer.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { articles } from '../Modules/articles'
 import Article from './Article'
+import { useState, useEffect } from 'react'
+import { RatioContext } from './RatioContext'
 
 function ArticleContainer() {
 
+    const [selectedid, setSelectedId] = useState(0)
+
+    var client_width = useContext(RatioContext)
+    
   return (
 
-    <div>
-        {articles.map((i, index)=>{
-            return <Article key={i.author} article={i}/>
-        })}
-        
+    <div className='articlecontainer'>
+        <nav className='nav_article'>
+            <h2 className='myarticle'>{client_width}</h2>
+            <legend>
+                <button onClick={()=>{(selectedid>=1) && setSelectedId(prev=>prev-1)}}>-</button>
+                <button onClick={()=>{(selectedid<articles.length-2) && setSelectedId(prev=>prev+1)}}>+</button>
+            </legend>
+        </nav>
+        <div className='article_flexcontainer'>
+            {articles.map((i, index)=>{
+                return <div style={selectedid?
+                     {transform:`translateX(${selectedid * -100}%)`, transition: `transform 1s ease`}
+                    :{transform:`translateX(${selectedid * 100}%)`, transition: `transform 1s ease`}}>
+                            <Article key={i.author} article={i}/>
+                       </div>
+            })}  
+        </div>
     </div>
+    
   )
 }
 
