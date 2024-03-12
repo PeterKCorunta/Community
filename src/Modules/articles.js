b097fd6fd57fbbc753548f39c5f715241071c127
Your Name~ <oruntakelechi86@gmail.com>
2024-03-12 16:52:30 +0100

    Update on the articles module file

--- src/Modules/articles.js
+++ src/Modules/articles.js
@@ -3,4 +3,6 @@ export const articles = [
     {id:2, title:"The Living Legacy", author:"Timothy Johns", description:"Living the best of today leaves us giving our best for tommorow"},
     {id:3, title:"The Journey", author:"Anonymous", description:"The journey of a thousand miles begins with a single step"},
     {id:4, title:"Time to Remember", author:"John Williams", description:"A good memory is like a key to the soul"},
+    {id:5, title:"Power of Choice", author:"John Sanchez", description:"The greatest power is the will to choose"},
+    {id:6, title:"Spiritual Wealth", author:"David Artem", description:"The inner content of a man is his greatest wealth"},
 ]
