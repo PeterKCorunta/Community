c9e770b0ebc7b34c645ca55229c39b9d2ec8709a
Your Name~ <oruntakelechi86@gmail.com>
2024-03-13 00:02:34 +0100

    Article Component Update early hrs on Wednesday 13/03

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { articles } from '../Modules/articles'
+import { useState, useContext } from 'react'
+import { IdContext } from './IdContext'
 
-function Article({article}) {
+function Article({article, handleSearch}) {
+    
+
   return (
     <div className='article_frame'>
         <h2 className='article_id'>{article.id}</h2>
-        <h3 className='article_title'><a className='article_linktitle' href='#article'>{article.title}</a></h3>
+        <h3 onClick={()=>{handleSearch(article.id)}} className='article_title'><a className='article_linktitle' href='#article'>{article.title}</a></h3>
         <p className='author_description'>{article.description}</p>
         <h3 className='author_title'>{article.author}</h3>
     </div>
