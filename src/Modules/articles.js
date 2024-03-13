738857202cdff6b3e1f034069169a772eaa1ef1d
Your Name~ <oruntakelechi86@gmail.com>
2024-03-13 18:57:41 +0100

    A new update on the module file articles.js

--- src/Modules/articles.js
+++ src/Modules/articles.js
@@ -1,8 +1,32 @@
 export const articles = [
-    {id:1, title:"The Ancestral Lineage", author:"Micheal J. Finch", description:"Reaching forth for the goal and holding onto the past will end up keeping one motionless"},
-    {id:2, title:"The Living Legacy", author:"Timothy Johns", description:"Living the best of today leaves us giving our best for tommorow"},
-    {id:3, title:"The Journey", author:"Anonymous", description:"The journey of a thousand miles begins with a single step"},
-    {id:4, title:"Time to Remember", author:"John Williams", description:"A good memory is like a key to the soul"},
-    {id:5, title:"Power of Choice", author:"John Sanchez", description:"The greatest power is the will to choose"},
-    {id:6, title:"Spiritual Wealth", author:"David Artem", description:"The inner content of a man is his greatest wealth"},
+    {id:1, title:"The Ancestral Lineage", author:"Micheal J. Finch", description:"Reaching forth for the goal and holding onto the past keeps one motionless",
+    lesson:`The power to set and achieve goals is inherent in man because he possesses a spirit, mind, soul and body. He has the
+    ability to recreate his world through the education of his mind. This is the first place where he envisions his goals and 
+    affirmations to succeed. It is always important though to remain focused and determined to actualise goals. True determination
+    is success.`},
+    {id:2, title:"The Living Legacy", author:"Timothy Johns", description:"Living the best of today leaves us giving our best for tommorow",
+    lesson:`One of the most harmful setbacks to goal setting is not letting go off past mistakes and regrets. This exploits 
+    mental and emotional energy leading to a weak attittude, damaged ego and reputation and ultimately low productivity. However, 
+    learning to manage your attitude is very important at this stage. Learning to be thankful and joyful in Christ can bolster your inner strength. Learn to forgive yourself. Learn to reach out to your
+    family and loved ones. Never take your challenges personal.`},
+    {id:3, title:"The Journey", author:"Anonymous", description:"The journey of a thousand miles begins with a single step",
+    lesson:`One of the most harmful setbacks to goal setting is not letting go off past mistakes and regrets. This exploits 
+    mental and emotional energy leading to a weak attittude, damaged ego and reputation and ultimately low productivity. However, 
+    learning to manage your attitude is very important at this stage. Learning to be thankful and joyful in Christ can bolster your inner strength. Learn to forgive yourself. Learn to reach out to your
+    family and loved ones. Never take your challenges personal.`},
+    {id:4, title:"Time to Remember", author:"John Williams", description:"A good memory is like a key to the soul",
+    lesson:`One of the most harmful setbacks to goal setting is not letting go off past mistakes and regrets. This exploits 
+    mental and emotional energy leading to a weak attittude, damaged ego and reputation and ultimately low productivity. However, 
+    learning to manage your attitude is very important at this stage. Learning to be thankful and joyful in Christ can bolster your inner strength. Learn to forgive yourself. Learn to reach out to your
+    family and loved ones. Never take your challenges personal.`},
+    {id:5, title:"Power of Choice", author:"John Sanchez", description:"The greatest power is the will to choose",
+    lesson:`One of the most harmful setbacks to goal setting is not letting go off past mistakes and regrets. This exploits 
+    mental and emotional energy leading to a weak attittude, damaged ego and reputation and ultimately low productivity. However, 
+    learning to manage your attitude is very important at this stage. Learning to be thankful and joyful in Christ can bolster your inner strength. Learn to forgive yourself. Learn to reach out to your
+    family and loved ones. Never take your challenges personal.`},
+    {id:6, title:"Spiritual Wealth", author:"David Artem", description:"The inner content of a man is his greatest wealth",
+    lesson:`One of the most harmful setbacks to goal setting is not letting go off past mistakes and regrets. This exploits 
+    mental and emotional energy leading to a weak attittude, damaged ego and reputation and ultimately low productivity. However, 
+    learning to manage your attitude is very important at this stage. Learning to be thankful and joyful in Christ can bolster your inner strength. Learn to forgive yourself. Learn to reach out to your
+    family and loved ones. Never take your challenges personal.`},
 ]
