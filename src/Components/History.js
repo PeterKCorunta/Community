b9096999a995b5c93d30425391c3e38dab915066
Your Name~ <oruntakelechi86@gmail.com>
2024-03-13 18:47:48 +0100

    A new History component highlighing the aim and vision of the website

---
+++ src/Components/History.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function History() {
+  return (
+    <section className='section_history'>
+        <h2 className='history_title'>HISTORY</h2>
+        <legend>
+            <p className='writeup'>Nothing comes closer to great achievements in our 
+            present-day society than reflecting on our past moral and intelluctual heritage. 
+            Since its inception in 2000, COMMUNITY has proven to be the the repository of records
+            of past artifacts and civilizations of African countries with its goal of promoting 
+            seemingly lost and viable cultures, goals and values of past centuries. A wise man once
+            said that the education of the youth is the beginning of government. In COMMUNITY, the idea
+            is to engage the youths to seek as well as interpret the moral and intellectual significance
+            of valuable historical records for their best educational interests.</p>
+        </legend>
+    </section>
+  )
+}
+
+export default History
