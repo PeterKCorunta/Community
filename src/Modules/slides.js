1b0c353a95694b704a8a0a61baea21130689a1cc
Your Name~ <oruntakelechi86@gmail.com>
2024-03-14 23:23:03 +0100

    First Update on slides.js

---
+++ src/Modules/slides.js
@@ -0,0 +1,14 @@
+import pic1 from '../Slides/pic1.jpg'
+import pic2 from '../Slides/pic2.jpg'
+import pic3 from '../Slides/pic3.jpg'
+import pic4 from '../Slides/pic4.jpg'
+import pic5 from '../Slides/pic5.jpg'
+
+export const slides = [
+    {id: 1, pic: pic1},
+    {id: 2, pic: pic2},
+    {id: 3, pic: pic3},
+    {id: 4, pic: pic4},
+    {id: 5, pic: pic5},
+    // {id: 6, pic: pic2},
+]
