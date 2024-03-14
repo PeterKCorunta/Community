701f521661daaf2fafe81734a4f9ad4a81536aa5
Your Name~ <oruntakelechi86@gmail.com>
2024-03-14 23:20:01 +0100

    Updates on History.js

--- src/Components/History.js
+++ src/Components/History.js
@@ -4,7 +4,7 @@ function History() {
   return (
     <section id='history' className='section_history'>
         <h2 className='history_title'>HISTORY</h2>
-        <legend>
+        <legend className='history_legend'>
             <p className='writeup'>Nothing comes closer to great achievements in our 
             present-day society than reflecting on our past moral and intelluctual heritage. 
             Since its inception in 2000, COMMUNITY has proven to be the the repository of records
