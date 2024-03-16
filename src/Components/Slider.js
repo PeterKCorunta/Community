960265f299938c226485b8ff18fe213333454f94
Your Name~ <oruntakelechi86@gmail.com>
2024-03-16 23:33:45 +0100

    Recent Update on Slider.js

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,16 +1,38 @@
 import React from 'react'
-import { slides } from '../Modules/slides'
+// import { slides } from '../Modules/slides'
+import { SlideContext } from '../Components/SlideContext';
+import { useContext} from 'react';
 
-function Slider({slide, imageno, nox}) {
+function Slider({slide, imageno, nox, slideUpdate, toggle}) {
+
+  const mypics = useContext(SlideContext)
+  // Calculate the total number of slides
+  // const totalSlides = slides.length;
+
+  // Calculate the index of the currently displayed slide
+  const currentIndex = imageno//(imageno % (totalSlides+1));
     
   return (
-    <div className={imageno<=(slide.id)?'slider_container':''}
-          style={imageno<=(slide.id)?{transform:`translateX(${((imageno * -100))}%)`, opacity: `1`, zIndex: `-999`, transition:`all 1s ease`}
-                         :{transform: `translateX(${((slides.length+(nox - imageno))) * (100 * (imageno)/(imageno))}%)`, zIndex: `-999`, transition:`all 1s ease`}}>
-                          
-        <img className="slide_img" src={slide.pic} alt={`Slide${slide.id}`}/>
-        {/* <h1>{((5+(nox - imageno)))}</h1> */}
-        {slide.id}
+    
+    <div
+      className="slider_container">
+      
+      
+      {mypics.map((slide, index) => (
+        <div key={index} id={slide.id} className="slide_item" onTransitionEnd={(e)=>{slideUpdate(mypics, currentIndex)}} style={(toggle===false)?{
+          transform: `translateX(${currentIndex * -100}%)`, // Translate based on currentIndex
+          transition: 'transform 0.5s ease',  // Add transition for smoother animation
+         }:
+        {transform: `translateX(${currentIndex * -100}%)`, // Translate based on currentIndex
+        transition: 'transform 0.5s ease', // Add transition for smoother animation
+      }}>
+          <img className="slide_img" src={slide.pic} alt={`Slide ${index + 1}`} />
+          <h1>{mypics.length}</h1>
+          
+        </div>
+      ))}
+
+      {console.log(mypics.length)}
     </div>
   )
 }
